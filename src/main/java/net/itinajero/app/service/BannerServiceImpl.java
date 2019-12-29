bc306e90ade2862c82e7ae7ad1c3fd1e07e123ad
joalsaoss <joalsaoss@gmail.com>
2019-12-29 14:05:27 -0500

    Update BannerServiceImpl.java
    Update BannerServiceImpl. Add file extension to name file.

--- src/main/java/net/itinajero/app/service/BannerServiceImpl.java
+++ src/main/java/net/itinajero/app/service/BannerServiceImpl.java
@@ -34,49 +34,49 @@ public class BannerServiceImpl implements IBannerService {
 			banner1.setId(1);
 			banner1.setTitulo("Slide 1");
 			banner1.setFecha(formatter.parse("19-11-2019"));
-			banner1.setArchivo("slide1");
+			banner1.setArchivo("slide1.jpg");
 			banner1.setStatus("Activo");
 			
 			Banner banner2 = new Banner();
 			banner2.setId(2);
 			banner2.setTitulo("Slide 2");
 			banner2.setFecha(formatter.parse("19-11-2019"));
-			banner2.setArchivo("slide2");
+			banner2.setArchivo("slide2.jpg");
 			banner2.setStatus("Activo");
 
 			Banner banner3 = new Banner();
 			banner3.setId(3);
 			banner3.setTitulo("Slide 3");
 			banner3.setFecha(formatter.parse("19-11-2019"));
-			banner3.setArchivo("slide3");
+			banner3.setArchivo("slide3.jpg");
 			banner3.setStatus("Activo");
 			
 			Banner banner4 = new Banner();
 			banner4.setId(4);
 			banner4.setTitulo("Slide 4");
 			banner4.setFecha(formatter.parse("19-11-2019"));
-			banner4.setArchivo("slide4");
+			banner4.setArchivo("slide4.jpg");
 			banner4.setStatus("Activo");
 			
 			Banner banner5 = new Banner();
 			banner5.setId(5);
 			banner5.setTitulo("Slide 5");
 			banner5.setFecha(formatter.parse("19-11-2019"));
-			banner5.setArchivo("slide5");
+			banner5.setArchivo("slide5.jpg");
 			banner5.setStatus("Activo");
 			
 			Banner banner6 = new Banner();
 			banner6.setId(6);
 			banner6.setTitulo("Slide 6");
 			banner6.setFecha(formatter.parse("19-11-2019"));
-			banner6.setArchivo("slide6");
+			banner6.setArchivo("slide6.jpg");
 			banner6.setStatus("Activo");
 			
 			Banner banner7 = new Banner();
 			banner7.setId(7);
 			banner7.setTitulo("Slide 7");
 			banner7.setFecha(formatter.parse("19-11-2019"));
-			banner7.setArchivo("slide7");
+			banner7.setArchivo("slide7.jpg");
 			banner7.setStatus("Activo");
 			
 			lista.add(banner1);
